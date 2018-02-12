667585658fcffdc896befc298bbd5dfe36f6c795
Celso Dantas <celsodantas@gmail.com>
2018-02-11 22:54:35 -0500

    first commit

---
+++ 2018-Playground/Assets/CameraRenderToTexture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRenderToTexture : MonoBehaviour {
+
+	private Camera cam = null;
+	private RenderTexture texture = null;
+
+	// // Use this for initialization
+	void Start () {
+		GameObject obj = GameObject.Find("PortalView");
+        cam = obj.GetComponent<Camera>();
+
+		texture = new RenderTexture(256, 256, 16);
+        texture.antiAliasing = 1;
+		texture.name = "CameraView" + GetInstanceID();
+        
+		cam.targetTexture = texture;
+	}
+
+	// Update is called once per frame
+	void Update () {
+        cam.Render();
+        
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.SetTexture("_Texture", texture);
+	}
+}
---
+++ 2018-Playground/Assets/color_flow.shader
@@ -0,0 +1,43 @@
+﻿Shader "Custom/color_flow" {
+	Properties {
+		_Intensity("Intensity", Range(0, 10)) = 1
+	}
+	SubShader {
+		Tags { "RenderType" = "Opaque" }
+
+		Pass {
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			half _Intensity;
+
+			struct OutPosition {
+				float4 pos : SV_POSITION;
+				float4 normal : NORMAL;
+			};
+
+			struct FragmentOutput {
+				float4 color : SV_TARGET;
+			};
+
+			OutPosition vert(float4 pos : POSITION, float4 normal : NORMAL) {
+				OutPosition o;
+				o.pos = UnityObjectToClipPos(pos);
+				o.normal = normal;
+
+				return o;
+			}
+
+			FragmentOutput frag(OutPosition pos) {
+				FragmentOutput o;
+				o.color = abs(_SinTime.w);
+
+				return o;
+			}
+
+			ENDCG
+		}
+	}
+}
